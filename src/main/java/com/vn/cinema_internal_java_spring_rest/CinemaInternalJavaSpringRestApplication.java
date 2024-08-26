36057e1fd94d802e47d2d972d146685fdfd3c38c
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-26 17:27:17 +0700

    hello world

--- src/main/java/com/vn/cinema_internal_java_spring_rest/CinemaInternalJavaSpringRestApplication.java
+++ src/main/java/com/vn/cinema_internal_java_spring_rest/CinemaInternalJavaSpringRestApplication.java
@@ -3,7 +3,12 @@ package com.vn.cinema_internal_java_spring_rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class CinemaInternalJavaSpringRestApplication {
 
 	public static void main(String[] args) {
