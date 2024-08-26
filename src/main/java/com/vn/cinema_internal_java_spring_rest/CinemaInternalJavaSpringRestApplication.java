38340c8d879932f9a6e0c3d8829eb79cf4dd3ffd
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-08-26 14:29:02 +0700

    init project

---
+++ src/main/java/com/vn/cinema_internal_java_spring_rest/CinemaInternalJavaSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.vn.cinema_internal_java_spring_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaInternalJavaSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaInternalJavaSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vn/cinema_internal_java_spring_rest/CinemaInternalJavaSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.cinema_internal_java_spring_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaInternalJavaSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
